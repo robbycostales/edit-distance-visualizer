9204a7477ce596226179ddbea4b6c7e70cdde274
Robby Costales <rcostales15@simons-rock.edu>
2019-04-11 15:26:28 -0400

    updated README

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# edit-distance-visualizer
+# Edit Distance Visualizer
+
 Visualizer for edit distance calculation
+
+Hosted at rcostales.com/edit-distance-visualizer
+
+Notes:
+- CSS / html head links are overkill because it used to be for my old website
+- This is NOT mobile friendly (I didn't take that into consideration when I made this)
--- index.php
+++ index.php
@@ -6,22 +6,39 @@
     <title>Edit Distance Visualizer</title>
     <meta name="description" content="Robby Costales">
 
-    <?php
-      // HEAD STUFF
-      $toHome = "../..";
-      include("$toHome/com/sub-head.php");
-    ?>
-  </head>
 
-  <?php
-    // NAVBAR
-    $navParam = "none";
-    include("$toHome/com/nav-min.php");
-  ?>
+    <!-- JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script
+      src="https://code.jquery.com/jquery-3.3.1.js"
+      integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+      crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <!-- font -->
+    <link href="https://fonts.googleapis.com/css?family=Exo+2" rel="stylesheet">
+
+    <!-- Add icon library -->
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no, width=device-width" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- specific css sheet -->
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+  </head>
 
   <body>
 
-    <div class="jumbotron jumbotron-fluid">
+    <div class="jumbotron">
       <div class="container">
         <h3>EDIT DISTANCE VISUALIZER</h3>
       </div>
@@ -240,11 +257,5 @@
     </script>
 
   </body>
-
-
-  <?php
-    // FOOTER
-    include("$toHome/com/footer-min.php");
-  ?>
 
 </html>
